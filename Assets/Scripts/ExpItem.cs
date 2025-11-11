2cc707f63ddd42572568a42a3126260907717cd8
이정환 <hwanlee2259@gmail.com>
2025-11-11 15:36:35 +0900

    #25 upgradeUI and install new tower

--- Assets/Scripts/ExpItem.cs
+++ Assets/Scripts/ExpItem.cs
@@ -4,6 +4,11 @@ public class ExpItem : DropItem
 {
     private float exp;
 
+    private bool isStartMove = false;
+
+    private float waitTimeInterval = 0.5f;
+    private float waitTime = 0f;
+
     protected override void OnEnable()
     {
         base.OnEnable();
@@ -19,12 +24,31 @@ public class ExpItem : DropItem
 
     protected override void Update()
     {
+        if (!isStartMove)
+        {
+            Waiting();
+            return;
+        }
+
         base.Update();
 
-        if(isDestroy)
+        if (isDestroy)
         {
             Destroy(gameObject);
             planet.CurrentExp += exp;
+        }
+    }
+    
+    private void Waiting()
+    {
+        if(waitTime < waitTimeInterval)
+        {
+            waitTime += Time.deltaTime;
+            return;
+        }
+        else
+        {
+            isStartMove = true;
         }
     }
 }
