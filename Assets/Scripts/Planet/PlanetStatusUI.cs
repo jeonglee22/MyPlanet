48e13a18515d17a2ed1a6a5105965e50fe0ebc8c
이정환 <hwanlee2259@gmail.com>
2025-11-11 10:26:23 +0900

    #24 add exp eventbus and test exp add

--- Assets/Scripts/Planet/PlanetStatusUI.cs
+++ Assets/Scripts/Planet/PlanetStatusUI.cs
@@ -1,16 +1,55 @@
+using System.IO;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlanetStatusUI : MonoBehaviour
 {
+    [SerializeField] private Planet planet;
+
+    [SerializeField] private Slider hpSlider;
+    [SerializeField] private Slider expSlider;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        hpSlider.onValueChanged.AddListener(OnHpValueChanged);
+        planet.expUpEvent += OnExpValueChange;
+
+        Initialize();
+        //test
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void OnDestroy()
+    {
+        planet.expUpEvent -= OnExpValueChange;
+    }
+
+    private void Initialize()
+    {
+        hpSlider.value = planet.Health;
+        expSlider.value = 0f;
+    }
+
+    private void OnHpValueChanged(float hpPercent)
+    {
+        hpSlider.value = hpPercent;
+    }
+
+    private void OnExpValueChange(float exp)
+    {
+        Debug.Log(exp);
+        expSlider.value = exp / planet.MaxExp;
+    }
+
+    public void AddExp()
+    {
+        planet.CurrentExp += 10f;
     }
 }
