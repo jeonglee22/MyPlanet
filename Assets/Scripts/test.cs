e651ca13e246a93da55742a943bcd6506fa5d085
이정환 <hwanlee2259@gmail.com>
2025-11-30 23:13:18 +0900

    check closest point test

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -5,21 +5,16 @@ public class test : MonoBehaviour
     void OnTriggerEnter(Collider other)
     {
         Debug.Log("Trigger Entered with " + other.gameObject.name);
+
+        Vector3 pos = other.ClosestPoint(transform.position);
+        Debug.Log($"Trigger Hit Pos: {pos}");
+
+        Debug.DrawLine(transform.position, pos, Color.red, 1f);
     }
 
     void OnCollisionEnter(Collision collision)
     {
         Debug.Log("Collision Entered with " + collision.gameObject.name);
         Debug.Log(collision.contacts.Length);
-    }
-
-    void Update()
-    {
-        if(!TouchManager.Instance.IsTouching)
-            return;
-            
-        Debug.Log("IsDragging : " + TouchManager.Instance.IsDragging.ToString());
-        Debug.Log("IsHolding : " + TouchManager.Instance.IsHolding.ToString());
-        Debug.Log("IsTouching : " + TouchManager.Instance.IsTouching.ToString());
     }
 }
