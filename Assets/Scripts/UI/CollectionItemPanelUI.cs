a57f5b401d46e31e9b40be752905dea80a0a5c54
서준규 <thddlwnsrb@naver.com>
2025-12-10 10:01:57 +0900

    change imagewhiteTextPrefab

---
+++ Assets/Scripts/UI/CollectionItemPanelUI.cs
@@ -0,0 +1,15 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectionItemPanelUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI itemNameText;
+    [SerializeField] private Image iconImg;
+    [SerializeField] private TextMeshProUGUI rateText;
+    [SerializeField] private TextMeshProUGUI weightText;
+    [SerializeField] private Button weightUpBtn;
+    [SerializeField] private Button weightDownBtn;
+
+    
+}
