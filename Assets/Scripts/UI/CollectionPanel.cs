ba7cc847257c2b22af9240718c4e78f3badf5a5f
Seo Jun Gyu <127666839+SeoJunGyu@users.noreply.github.com>
2025-12-16 01:00:48 +0900

    sort collection panel

--- Assets/Scripts/UI/CollectionPanel.cs
+++ Assets/Scripts/UI/CollectionPanel.cs
@@ -158,6 +158,7 @@ public class CollectionPanel : MonoBehaviour
 
         UpdateCoreText();
         RefreshAllWeights();
+        SortPanelsWeight();
 
         InfoPanelClosed();
     }
@@ -172,6 +173,7 @@ public class CollectionPanel : MonoBehaviour
 
         UpdateCoreText();
         RefreshAllWeights();
+        SortPanelsWeight();
 
         InfoPanelClosed();
     }
@@ -211,6 +213,8 @@ public class CollectionPanel : MonoBehaviour
         backBtn.interactable = true;
 
         UpdateCoreText();
+
+        SortPanelsWeight();
 
         if(isFromBackButton)
         {
@@ -280,6 +284,8 @@ public class CollectionPanel : MonoBehaviour
         isAbilityPanel = false;
 
         UpdateCoreText();
+
+        SortPanelsWeight();
     }
 
     private void InitializeTowerPanel()
@@ -467,6 +473,120 @@ public class CollectionPanel : MonoBehaviour
         towerInfoPanelObj.SetActive(false);
         buffTowerInfoPanelObj.SetActive(false);
         randomAbilityInfoPanelObj.SetActive(false);
+    }
+
+    private void SortPanelsWeight()
+    {
+        if (isTowerPanel)
+        {
+            SortTowerPanels();
+        }
+        else if (isAbilityPanel)
+        {
+            SortAbilityPanels();
+        }
+    }
+
+    private void SortTowerPanels()
+    {
+        var towerPanels = new List<CollectionItemPanelUI>();
+
+        foreach(var panel in allPanels)
+        {
+            if(panel.IsTower)
+            {
+                towerPanels.Add(panel);
+            }
+        }
+
+        towerPanels.Sort((a, b) =>
+        {
+            int aId = a.IsAttackTower ? a.AttackTowerData.AttackTower_Id : a.BuffTowerData.BuffTower_ID;
+            int bId = b.IsAttackTower ? b.AttackTowerData.AttackTower_Id : b.BuffTowerData.BuffTower_ID;
+
+            float aWeight = CollectionManager.Instance.GetWeight(aId);
+            float bWeight = CollectionManager.Instance.GetWeight(bId);
+
+            return bWeight.CompareTo(aWeight);
+        });
+
+        var rows = new List<Transform>();
+        foreach(var item in instantiatedItems)
+        {
+            if(item.transform.parent == towerPanelContent.transform)
+            {
+                rows.Add(item.transform);
+            }
+        }
+
+        for(int i = 0; i < towerPanels.Count; i++)
+        {
+            int rowIndex = i / 2;
+            int colIndex = i % 2;
+
+            if(rowIndex < rows.Count)
+            {
+                var emptyGameObject = towerPanels[i].transform.parent;
+                emptyGameObject.SetParent(rows[rowIndex]);
+                emptyGameObject.SetSiblingIndex(colIndex);
+            }
+        }
+
+        for(int i = 0; i < rows.Count; i++)
+        {
+            rows[i].SetSiblingIndex(i);
+        }
+    }
+
+    private void SortAbilityPanels()
+    {
+        var abilityPanels = new List<CollectionItemPanelUI>();
+
+        foreach(var panel in allPanels)
+        {
+            if(!panel.IsTower)
+            {
+                abilityPanels.Add(panel);
+            }
+        }
+
+        abilityPanels.Sort((a, b) =>
+        {
+            int aId = a.AbilityData.RandomAbility_ID;
+            int bId = b.AbilityData.RandomAbility_ID;
+
+            float aWeight = CollectionManager.Instance.GetWeight(aId);
+            float bWeight = CollectionManager.Instance.GetWeight(bId);
+
+            return bWeight.CompareTo(aWeight);
+        });
+
+        var rows = new List<Transform>();
+        foreach(var item in instantiatedItems)
+        {
+            if(item.transform.parent == abilityPanelContent.transform)
+            {
+                rows.Add(item.transform);
+            }
+        }
+
+        for(int i = 0; i < abilityPanels.Count; i++)
+        {
+            int rowIndex = i / 2;
+            int colIndex = i % 2;
+
+            if(rowIndex < rows.Count)
+            {
+                var emptyGameObject = abilityPanels[i].transform.parent;
+                emptyGameObject.SetParent(rows[rowIndex]);
+                emptyGameObject.SetSiblingIndex(colIndex);
+            }
+        }
+
+        for(int i = 0; i < rows.Count; i++)
+        {
+            rows[i].SetSiblingIndex(i);
+        }
     }
 
 }
